f3cfb60ad657303fddc3fb8ab52f4f0cd346efc7
Kaya <ubersensei@gmail.com>
2019-04-06 19:44:24 +0800

    with Ion

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,23 @@ import OtherPage from './OtherPage';
 import Users from './Users';
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <header className="App-header">                      
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
-          </header>
-          <div>
-            <h1>Hola ye!</h1>
-            <Route exact path="/" component={Users} />
-            <Route path="/otherpage" component={OtherPage} />
-          </div>
-        </div>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <header className="App-header">
+                        <Link to="/">Home</Link>
+                        <Link to="/otherpage">Other Page</Link>
+                    </header>
+                    <div>
+                        <h1>Hola ye! with Ion</h1>
+                        <Route exact path="/" component={Users} />
+                        <Route path="/otherpage" component={OtherPage} />
+                    </div>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
